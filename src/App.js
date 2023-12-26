eb264e8c1dc415cbb0e0619844d056f48e9bdb10
김선영 <kimksy27@gmail.com>
2023-12-27 01:46:53 +0900

    COUNTER REACT

--- src/App.js
+++ src/App.js
@@ -3,39 +3,19 @@ import React from 'react';    // React 파일 불러오기
 import './App.css';
 
 import MyHeader from './MyHeader';
+import Counter from './Counter';
 
 
 function App() {
-  let name = "김선영";
 
-  // css 파일을 사용하지 않고 inline 스타일 사용 가능
-  const style = {   
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "yellow",
-    },
-    bold_text: {
-      color: "green",
-    },
-  };
 
   const number = 5;
 
   return (
-    <div style={style.App}>
-      <MyHeader/>
-      <h2 style={style.h2}>안녕 리액트 {name} </h2>
-      {/* {} 안에 숫자나 문자 넣어도 가능 */}
-      {/* 단, flase나 배열같은거 넣으면 렌더 안됨 */}
-      {/* jsx의 중괄호 안에는 어떤 값을 포함할 수 있지만 숫자나 문자열만 포함할 수 있다 */}
+    <div>
+      <MyHeader />
+    <Counter />
 
-      <b style={style.bold_text} id="bold_text">
-      {number}는 {number % 2 === 0 ? '짝수' : '홀수'}
-      {/* jsx는 렌더링할 떄 조건에 따라 다른 요소 렌더링 가능 */}
-      
-      </b>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,45 @@
+import React,{useState} from "react";
+
+// state는 리액트의 기능이기 때문에 임포트를 해줘야함
+// useState 상태를 사용하겠다
+
 const Counter = () => {
+  // 0에서 출발
+  // 1씩 증가하고
+  // 1씩 감소하는
+  // count 상태
+
+  const [count, setCount] = useState(0);  // 초기값은 0
+
+  const onIncrease = () => {
+    setCount(count +1);
+  }
+
+  const onDecrease = () => {
+    setCount(count -1);
+  }
+
+  const [count2, setCount2] = useState(0);  // 0부터 시작
+
+  const onIncrease2 = () => {
+    setCount2(count2 +1);
+  }
+
+  const onDecrease2 = () => {
+    setCount2(count2 -1);
+  }
+
   return (
     <div>
-      <h2>0</h2>
-      <button>+</button>
-      <button>-</button>
+      <h2>{count}</h2>
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+
+      <h2>{count2}</h2>
+      <button onClick={onIncrease2}>+</button>
+      <button onClick={onDecrease2}>-</button>
     </div>
   )
 }
 
-export default Counter;s
+export default Counter;
